afa850e444700c46bb3a446540707bd560df6636
seobkeom <ajm153123@naver.com>
2023-05-27 23:23:50 +0900

    package update

---
+++ frontend/src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p v-if="isShow">Superpil is awesome!</p>
+    <p v-if="isNoShow">No Show</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: true,
+      isNoShow: false,
+    };
+  },
+};
+</script>
